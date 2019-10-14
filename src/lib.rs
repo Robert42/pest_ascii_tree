7f5074f8cb67e4ee7aaf4886365de8881c351327
Robert Hildebrandt <robert.3dgamedev@gmail.com>
2019-10-14 10:50:46 +0200

    applied `cargo fmt`

--- src/lib.rs
+++ src/lib.rs
@@ -32,18 +32,18 @@
 //!
 //! [1]: https://crates.io/crates/ascii_tree
 
-
 extern crate ascii_tree;
-extern crate pest;
 extern crate escape_string;
+extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-use pest::{iterators::Pairs, error::Error};
+use pest::{error::Error, iterators::Pairs};
 
-fn as_ascii_tree_nodes<R>(mut pairs: Pairs<R>) -> Vec<ascii_tree::Tree> where
-    R: pest::RuleType {
-
+fn as_ascii_tree_nodes<R>(mut pairs: Pairs<R>) -> Vec<ascii_tree::Tree>
+where
+    R: pest::RuleType,
+{
     let mut vec = Vec::new();
 
     while let Some(pair) = pairs.next() {
@@ -58,7 +58,11 @@ fn as_ascii_tree_nodes<R>(mut pairs: Pairs<R>) -> Vec<ascii_tree::Tree> where
 
         let node;
         if inner_pairs.is_empty() {
-            let leaf = vec![format!("{:?} \"{}\"", pair_rule, escape_string::escape(pair_content))];
+            let leaf = vec![format!(
+                "{:?} \"{}\"",
+                pair_rule,
+                escape_string::escape(pair_content)
+            )];
             node = ascii_tree::Tree::Leaf(leaf);
         } else {
             node = ascii_tree::Tree::Node(rule_name, inner_pairs);
@@ -98,16 +102,19 @@ fn as_ascii_tree_nodes<R>(mut pairs: Pairs<R>) -> Vec<ascii_tree::Tree> where
 ///
 /// # Error
 /// If the internal call to `ascii_tree::write_tree` failed, the error is passed to the caller.
-pub fn as_ascii_tree<R>(pairs: Pairs<R>) -> Result<String, std::fmt::Error> where
-    R: pest::RuleType {
-
+pub fn as_ascii_tree<R>(pairs: Pairs<R>) -> Result<String, std::fmt::Error>
+where
+    R: pest::RuleType,
+{
     let nodes = as_ascii_tree_nodes(pairs);
 
     let mut output = String::new();
 
     match nodes.len() {
-        0 => {},
-        1 => {ascii_tree::write_tree(&mut output, nodes.first().unwrap())?;},
+        0 => {}
+        1 => {
+            ascii_tree::write_tree(&mut output, nodes.first().unwrap())?;
+        }
         _ => {
             let root = ascii_tree::Tree::Node(String::new(), nodes);
             ascii_tree::write_tree(&mut output, &root)?;
@@ -157,14 +164,17 @@ pub fn as_ascii_tree<R>(pairs: Pairs<R>) -> Result<String, std::fmt::Error> wher
 ///  └─ val "z"
 /// </pre>
 ///
-pub fn print_as_ascii_tree<R>(parsing_result : Result<Pairs<R>, Error<R>>) where
-    R: pest::RuleType {
-
+pub fn print_as_ascii_tree<R>(parsing_result: Result<Pairs<R>, Error<R>>)
+where
+    R: pest::RuleType,
+{
     match parsing_result {
-        Ok(pairs) => {
-            match as_ascii_tree(pairs) {
-                Ok(output) => {println!("{}", output);}
-                Err(e) => {eprintln!("{}", e);}
+        Ok(pairs) => match as_ascii_tree(pairs) {
+            Ok(output) => {
+                println!("{}", output);
+            }
+            Err(e) => {
+                eprintln!("{}", e);
             }
         },
         Err(e) => {
@@ -173,12 +183,11 @@ pub fn print_as_ascii_tree<R>(parsing_result : Result<Pairs<R>, Error<R>>) where
     }
 }
 
-
 #[cfg(test)]
 mod tests {
 
-    use pest::Parser;
     use super::as_ascii_tree;
+    use pest::Parser;
 
     #[derive(Parser)]
     #[grammar = "expression.pest"]
@@ -186,84 +195,103 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = as_ascii_tree(ExpressionParser::parse(Rule::expr, "a + b + c").unwrap()).unwrap();
-        assert_eq!(result,
-                   String::new() +
-                   " expr\n" +
-                   " ├─ val \"a\"\n" +
-                   " ├─ op \"+\"\n" +
-                   " ├─ val \"b\"\n" +
-                   " ├─ op \"+\"\n" +
-                   " └─ val \"c\"\n");
+        let result =
+            as_ascii_tree(ExpressionParser::parse(Rule::expr, "a + b + c").unwrap()).unwrap();
+        assert_eq!(
+            result,
+            String::new()
+                + " expr\n"
+                + " ├─ val \"a\"\n"
+                + " ├─ op \"+\"\n"
+                + " ├─ val \"b\"\n"
+                + " ├─ op \"+\"\n"
+                + " └─ val \"c\"\n"
+        );
 
-        let result = as_ascii_tree(ExpressionParser::parse(Rule::expr_root, "x + y + z").unwrap()).unwrap();
-        assert_eq!(result,
-                   String::new() +
-                   " ├─ val \"x\"\n" +
-                   " ├─ op \"+\"\n" +
-                   " ├─ val \"y\"\n" +
-                   " ├─ op \"+\"\n" +
-                   " └─ val \"z\"\n");
+        let result =
+            as_ascii_tree(ExpressionParser::parse(Rule::expr_root, "x + y + z").unwrap()).unwrap();
+        assert_eq!(
+            result,
+            String::new()
+                + " ├─ val \"x\"\n"
+                + " ├─ op \"+\"\n"
+                + " ├─ val \"y\"\n"
+                + " ├─ op \"+\"\n"
+                + " └─ val \"z\"\n"
+        );
 
         let result = as_ascii_tree(ExpressionParser::parse(Rule::val, "m").unwrap()).unwrap();
-        assert_eq!(result,
-                   String::new() +
-                   " val \"m\"\n");
+        assert_eq!(result, String::new() + " val \"m\"\n");
 
-        let result = as_ascii_tree(ExpressionParser::parse(Rule::expr, "(u + (v + w)) + (x + y) + z").unwrap()).unwrap();
-        assert_eq!(result,
-                   String::new() +
-                   " expr\n" +
-                   " ├─ expr\n" +
-                   " │  ├─ val \"u\"\n" +
-                   " │  ├─ op \"+\"\n" +
-                   " │  └─ expr\n" +
-                   " │     ├─ val \"v\"\n" +
-                   " │     ├─ op \"+\"\n" +
-                   " │     └─ val \"w\"\n" +
-                   " ├─ op \"+\"\n" +
-                   " ├─ expr\n" +
-                   " │  ├─ val \"x\"\n" +
-                   " │  ├─ op \"+\"\n" +
-                   " │  └─ val \"y\"\n" +
-                   " ├─ op \"+\"\n" +
-                   " └─ val \"z\"\n");
+        let result = as_ascii_tree(
+            ExpressionParser::parse(Rule::expr, "(u + (v + w)) + (x + y) + z").unwrap(),
+        )
+        .unwrap();
+        assert_eq!(
+            result,
+            String::new()
+                + " expr\n"
+                + " ├─ expr\n"
+                + " │  ├─ val \"u\"\n"
+                + " │  ├─ op \"+\"\n"
+                + " │  └─ expr\n"
+                + " │     ├─ val \"v\"\n"
+                + " │     ├─ op \"+\"\n"
+                + " │     └─ val \"w\"\n"
+                + " ├─ op \"+\"\n"
+                + " ├─ expr\n"
+                + " │  ├─ val \"x\"\n"
+                + " │  ├─ op \"+\"\n"
+                + " │  └─ val \"y\"\n"
+                + " ├─ op \"+\"\n"
+                + " └─ val \"z\"\n"
+        );
 
-        let result = as_ascii_tree(ExpressionParser::parse(Rule::root, "(u + (v + w)) + (x + y) + z").unwrap()).unwrap();
-        assert_eq!(result,
-                   String::new() +
-                   " expr\n" +
-                   " ├─ expr\n" +
-                   " │  ├─ val \"u\"\n" +
-                   " │  ├─ op \"+\"\n" +
-                   " │  └─ expr\n" +
-                   " │     ├─ val \"v\"\n" +
-                   " │     ├─ op \"+\"\n" +
-                   " │     └─ val \"w\"\n" +
-                   " ├─ op \"+\"\n" +
-                   " ├─ expr\n" +
-                   " │  ├─ val \"x\"\n" +
-                   " │  ├─ op \"+\"\n" +
-                   " │  └─ val \"y\"\n" +
-                   " ├─ op \"+\"\n" +
-                   " └─ val \"z\"\n");
+        let result = as_ascii_tree(
+            ExpressionParser::parse(Rule::root, "(u + (v + w)) + (x + y) + z").unwrap(),
+        )
+        .unwrap();
+        assert_eq!(
+            result,
+            String::new()
+                + " expr\n"
+                + " ├─ expr\n"
+                + " │  ├─ val \"u\"\n"
+                + " │  ├─ op \"+\"\n"
+                + " │  └─ expr\n"
+                + " │     ├─ val \"v\"\n"
+                + " │     ├─ op \"+\"\n"
+                + " │     └─ val \"w\"\n"
+                + " ├─ op \"+\"\n"
+                + " ├─ expr\n"
+                + " │  ├─ val \"x\"\n"
+                + " │  ├─ op \"+\"\n"
+                + " │  └─ val \"y\"\n"
+                + " ├─ op \"+\"\n"
+                + " └─ val \"z\"\n"
+        );
 
-        let result = as_ascii_tree(ExpressionParser::parse(Rule::expr_root, "(u + (v + w)) + (x + y) + z").unwrap()).unwrap();
-        assert_eq!(result,
-                   String::new() +
-                   " ├─ expr\n" +
-                   " │  ├─ val \"u\"\n" +
-                   " │  ├─ op \"+\"\n" +
-                   " │  └─ expr\n" +
-                   " │     ├─ val \"v\"\n" +
-                   " │     ├─ op \"+\"\n" +
-                   " │     └─ val \"w\"\n" +
-                   " ├─ op \"+\"\n" +
-                   " ├─ expr\n" +
-                   " │  ├─ val \"x\"\n" +
-                   " │  ├─ op \"+\"\n" +
-                   " │  └─ val \"y\"\n" +
-                   " ├─ op \"+\"\n" +
-                   " └─ val \"z\"\n");
+        let result = as_ascii_tree(
+            ExpressionParser::parse(Rule::expr_root, "(u + (v + w)) + (x + y) + z").unwrap(),
+        )
+        .unwrap();
+        assert_eq!(
+            result,
+            String::new()
+                + " ├─ expr\n"
+                + " │  ├─ val \"u\"\n"
+                + " │  ├─ op \"+\"\n"
+                + " │  └─ expr\n"
+                + " │     ├─ val \"v\"\n"
+                + " │     ├─ op \"+\"\n"
+                + " │     └─ val \"w\"\n"
+                + " ├─ op \"+\"\n"
+                + " ├─ expr\n"
+                + " │  ├─ val \"x\"\n"
+                + " │  ├─ op \"+\"\n"
+                + " │  └─ val \"y\"\n"
+                + " ├─ op \"+\"\n"
+                + " └─ val \"z\"\n"
+        );
     }
 }
